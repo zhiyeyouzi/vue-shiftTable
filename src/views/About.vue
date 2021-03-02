448b65ed3cb90a8334bee2dabf49df729f306e2c
zhiyeyouzi <49635947+zhiyeyouzi@users.noreply.github.com>
2021-03-02 19:13:38 +0800

    Update About.vue
    禁止选中文件样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -337,6 +337,12 @@ export default {
 
 .week-home{
   padding:  0 10px 10px 10px;
+   //禁止文本被选中
+  -moz-user-select:none; /*火狐*/
+  -webkit-user-select:none; /*webkit浏览器*/
+  -ms-user-select:none; /*IE10*/
+  -khtml-user-select:none; /*早期浏览器*/
+  user-select:none;
 }
 
 //占位用
